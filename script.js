f0be89855ead2ff05a92a5afe242bde99853785c
Paulo Victor <paulo16xx@hotmail.com>
2022-04-19 18:31:32 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,75 @@
+/*
+  !declaração de variaveis
+*/
+let modalQt = 1;
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
+
+/*
+?Map() do json externo para manipulação das propriedades, cloneNode() para clonar os modais
+*/
+
+pizzaJson.map((item, index) => {
+  let pizzaItem = c(".models .pizza-item").cloneNode(true);
+
+  pizzaItem.setAttribute("data-key", index);
+  pizzaItem.querySelector(".pizza-item--img img").src = item.img;
+  pizzaItem.querySelector(
+    ".pizza-item--price"
+  ).innerHTML = `${item.price.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  })}`;
+  pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
+  pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+
+  /*
+  ?Previne o padrão do link para não atualizar a pagina
+  */
+
+  pizzaItem.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
+    let key = e.target.closest(".pizza-item").getAttribute("data-key");
+    modalQt = 1;
+
+    /*
+    ?Ajustes do modal como a foto, tamanho, preço etc...
+    */
+
+    c(".pizzaBig img").src = pizzaJson[key].img;
+    c(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
+    c(".pizzaInfo--desc").innerHTML = pizzaJson[key].description;
+    c(".pizzaInfo--actualPrice").innerHTML = pizzaJson[key].price.toFixed(2);
+    c(".pizzaInfo--size.selected").classList.remove("selected");
+
+    /*
+    ?ForEach para as add as gramas e "Resetar" o btn do tamanho selecionado
+    */
+
+    cs(".pizzaInfo--size").forEach((size, sizeIndex) => {
+      if (sizeIndex == 2) {
+        size.classList.add("selected");
+      }
+
+      size.querySelector("span").innerHTML = pizzaJson[key].sizes[sizeIndex];
+    });
+
+    c(".pizzaInfo--qt").innerHTML = modalQt;
+
+    /*
+    ?Faz surgir o modal e cria efeito fade
+    */
+
+    c(".pizzaWindowArea").style.opacity = 0;
+    c(".pizzaWindowArea").style.display = "flex";
+    setTimeout(() => {
+      c(".pizzaWindowArea").style.opacity = 1;
+    }, 200);
+  });
+
+  c(".pizza-area").append(pizzaItem);
+});
+
+/*
+Eventos do modal
+*/ 
